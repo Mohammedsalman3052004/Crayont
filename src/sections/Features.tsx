473e032ba0a1e1d03ee2165e0221a437a01d7db6
Mohammedsalman3052004 <mdsalman3052004@gmail.com>
2025-01-30 23:51:10 +0530

    error solved

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,13 +1,4 @@
-const features = [
-    "Asset Library",
-    "Code Preview",
-    "Flow Mode",
-    "Smart Sync",
-    "Auto Layout",
-    "Fast Search",
-    "Smart Guides",
-];
 
-export default function Features() {
-    return <div>Features</div>;
-}
+// export default function Features() {
+//     return <div>Features</div>;
+// }
--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,6 +1,6 @@
 import Tag from "@/components/Tag";
 
-const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+const text = `You&apos;re racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
 
 export default function Introduction() {
     return(
@@ -12,7 +12,7 @@ export default function Introduction() {
                 <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
                     <span>Your creative process deserves better.</span>{" "}
                     <span className="text-white/15">{text}</span>
-                    <span className="text-lime-400 block">That's why we built Layers.</span>
+                    <span className="text-lime-400 block">That&apos;s why we built Layers.</span>
                 </div>
             </div>
         </section>
